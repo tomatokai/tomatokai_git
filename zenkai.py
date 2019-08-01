3488aef947168a1e79cca89d4ccefeb7d381ac64
tomatokai <zeso@qq.com>
2019-08-01 10:49:25 +0800

    增加zenkai工具庫,easyGet用於方便獲取文件夾內的文件名

---
+++ zenkai.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# config: utf-8
+
+import os
+import shutil
+import pymongo
+
+#用來獲取文件名
+class easyGet():
+	def get_file_name(file_path,file_format,word_bin,word_end):
+		L = []
+		for root,dirs,files in os.walk(file_path):
+			if os.path.splitext(file)[1] == file_format:#控制讀取格式
+				L.append(os.path.join(root,file))
+		list_new = []
+		for i in L:
+			i = i[word_bin:word_end]
+			list_new.append(i)
+		return list_new
